596a396fd7a8e8f71432c4c5796ae87d7b678e8c
oletemani <oletemani@gmail.com>
2022-05-17 15:25:16 +0530

    Moved to SearchFilter Folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import SearchFilter from './SearchFilter'
+import SearchFilter from './SearchFilter/SearchFilter'
 class App extends React.Component {
     render() {
         return <div>
